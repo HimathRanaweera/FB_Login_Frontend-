c940475c2af585e8306851976cade0d997369677
Himath Ranaweera <145730950+HimathRanaweera@users.noreply.github.com>
2024-02-18 11:31:22 +0530

    Loging Page UI

---
+++ FB_LOGIN/src/login.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './login.css'
+
+function login() {
+  return (
+    <>
+        <div className="content">
+                <div className="flex-div">
+                    <div className="name-content">
+                        <h1 className="logo">Facebook</h1>
+                        <p>Connect with friends and the world around you on Facebook.</p>
+                    </div>
+                    <form>
+                        <input type="text" placeholder="Email or Phone Number" required />
+                        <input type="password" placeholder="Password" required />
+                        <button className="login">Log In</button>
+                        <a href="#">Forgot Password ?</a>
+                        <hr />
+                        <button className="create-account">Create New Account</button>
+                    </form>
+                </div>
+            </div>
+    </>
+  )
+}
+
+export default login
